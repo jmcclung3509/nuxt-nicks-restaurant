42c9589c9e713c4e9382f41f7c2329f3c3439828
Jessica Turner <jmcclung3509@gmail.com>
2023-03-10 13:51:07 -0800

    removed underline link from child-links

--- components/HeaderMobile.vue
+++ components/HeaderMobile.vue
@@ -7,44 +7,58 @@
 					<template #visible="visibleProps">
 						<nuxt-link to="/menu" class="parent-link underline-link">Menu</nuxt-link>
 					</template>
-					<template #hidden>
+					<template #hidden ="{ closeAccordion }">
 						<div class="flex flex-col">
 
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true"
+							<nuxt-link
+							@click="closeAccordion" class="child-link whitespace-nowrap" :external="true"
 								to="/menu#breakfast">Breakfast & Lunch</nuxt-link>
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true"
+							<nuxt-link
+							@click="closeAccordion" class="child-link whitespace-nowrap" :external="true"
 								to="/menu#dinner">Dinner</nuxt-link>
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true"
+							<nuxt-link
+							@click="closeAccordion"
+							class="child-link whitespace-nowrap" :external="true"
 								to="/menu#dessert">Dessert</nuxt-link>
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true" to="/menu#bar">Bar &
+							<nuxt-link
+							@click="closeAccordion"
+							class="child-link whitespace-nowrap" :external="true" to="/menu#bar">Bar &
 								Lounge</nuxt-link>
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true" to="/menu#happy-hour">Happy
+							<nuxt-link
+							@click="closeAccordion"
+							class="child-link whitespace-nowrap" :external="true" to="/menu#happy-hour">Happy
 								Hour</nuxt-link>
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true"
+							<nuxt-link
+							@click="closeAccordion"
+							class="child-link whitespace-nowrap" :external="true"
 								to="/menu#banquet">Banquets</nuxt-link>
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true" to="/menu#music">Live
+							<nuxt-link
+							@click="closeAccordion"
+							class="child-link whitespace-nowrap" :external="true" to="/menu#music">Live
 								Music</nuxt-link>
 
 						</div>
 					</template>
 				</Accordion>
 			</li>
-			<li><nuxt-link to="/about"  class="parent-link underline-link">About Us</nuxt-link></li>
-			<li><nuxt-link to="/seabreeze"  class="parent-link underline-link">SeaBreeze</nuxt-link></li>
+			<li><nuxt-link
+			 to="/about"  class="parent-link underline-link">About Us</nuxt-link></li>
+			<li><nuxt-link
+			 to="/seabreeze"  class="parent-link underline-link">SeaBreeze</nuxt-link></li>
 			<li>
 				<Accordion :id="1" :open="data.accordionPages[1].open" @toggleAccordion="onAccordionClick">
 					<template #visible="visibleProps">
 						<nuxt-link to="/gallery" class="parent-link underline-link">
 							Gallery</nuxt-link>
 					</template>
-					<template #hidden>
+					<template #hidden="{ closeAccordion }">
 						<div class="flex flex-col">
 
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true"
+							<nuxt-link @click="closeAccordion" class="child-link whitespace-nowrap" :external="true"
 								to="/gallery#views">Views</nuxt-link>
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true" to="/gallery#food">Menu
+							<nuxt-link @click="closeAccordion" class="child-link whitespace-nowrap" :external="true" to="/gallery#food">Menu
 								Items</nuxt-link>
-							<nuxt-link class="child-link underline-link whitespace-nowrap" :external="true" to="/gallery#fun">Fun
+							<nuxt-link @click="closeAccordion" class="child-link whitespace-nowrap" :external="true" to="/gallery#fun">Fun
 								Times</nuxt-link>
 
 						</div>
