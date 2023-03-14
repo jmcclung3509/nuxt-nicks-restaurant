6448765d940ed71f9b31ee9ea93bd99713763287
Jessica Turner <jmcclung3509@gmail.com>
2023-03-14 08:03:27 -0700

    Mobile Menu Styles

--- components/HeaderMobile.vue
+++ components/HeaderMobile.vue
@@ -1,85 +1,140 @@
-
 <template>
-	<div class="mobile-menu flex flex-col lg:hidden overflow-auto space-y-4 absolute">
+	<div
+		class="mobile-menu flex flex-col lg:hidden overflow-auto space-y-4 absolute"
+		:class="{ 'mobile-menu-open': data.isMobileOpen }">
 		<ul>
 			<li>
-				<Accordion id="0" :open="data.accordionPages[0].open" @toggleAccordion="onAccordionClick">
+				<Accordion
+					id="0"
+					:open="data.accordionPages[0].open"
+					@toggleAccordion="onAccordionClick">
 					<template #visible="visibleProps">
-						<nuxt-link to="/menu" class="parent-link underline-link">Menu</nuxt-link>
+						<nuxt-link
+							to="/menu"
+							class="parent-link underline-link"
+							>Menu</nuxt-link
+						>
 					</template>
-					<template #hidden ="{ closeAccordion }">
+					<template #hidden>
 						<div class="flex flex-col">
-
 							<nuxt-link
-							@click="closeAccordion" class="child-link whitespace-nowrap" :external="true"
-								to="/menu#breakfast">Breakfast & Lunch</nuxt-link>
+								@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/menu#breakfast"
+								>Breakfast & Lunch</nuxt-link
+							>
 							<nuxt-link
-							@click="closeAccordion" class="child-link whitespace-nowrap" :external="true"
-								to="/menu#dinner">Dinner</nuxt-link>
+								@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/menu#dinner"
+								>Dinner</nuxt-link
+							>
 							<nuxt-link
-							@click="closeAccordion"
-							class="child-link whitespace-nowrap" :external="true"
-								to="/menu#dessert">Dessert</nuxt-link>
+								@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/menu#dessert"
+								>Dessert</nuxt-link
+							>
 							<nuxt-link
-							@click="closeAccordion"
-							class="child-link whitespace-nowrap" :external="true" to="/menu#bar">Bar &
-								Lounge</nuxt-link>
+								@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/menu#bar"
+								>Bar & Lounge</nuxt-link
+							>
 							<nuxt-link
-							@click="closeAccordion"
-							class="child-link whitespace-nowrap" :external="true" to="/menu#happy-hour">Happy
-								Hour</nuxt-link>
+								@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/menu#happy-hour"
+								>Happy Hour</nuxt-link
+							>
 							<nuxt-link
-							@click="closeAccordion"
-							class="child-link whitespace-nowrap" :external="true"
-								to="/menu#banquet">Banquets</nuxt-link>
+								@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/menu#banquet"
+								>Banquets</nuxt-link
+							>
 							<nuxt-link
-							@click="closeAccordion"
-							class="child-link whitespace-nowrap" :external="true" to="/menu#music">Live
-								Music</nuxt-link>
-
+								@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/menu#music"
+								>Live Music</nuxt-link
+							>
 						</div>
 					</template>
 				</Accordion>
 			</li>
-			<li><nuxt-link
-			 to="/about"  class="parent-link underline-link">About Us</nuxt-link></li>
-			<li><nuxt-link
-			 to="/seabreeze"  class="parent-link underline-link">SeaBreeze</nuxt-link></li>
 			<li>
-				<Accordion :id="1" :open="data.accordionPages[1].open" @toggleAccordion="onAccordionClick">
+				<nuxt-link
+					to="/about"
+					class="parent-link underline-link"
+					>About Us</nuxt-link
+				>
+			</li>
+			<li>
+				<a
+					class="underline-link parent-link"
+					href="https://www.seabreezemotel.net"
+					target="_blank">
+					Sea Breeze Motel</a
+				>
+			</li>
+			<li>
+				<Accordion
+					:id="1"
+					:open="data.accordionPages[1].open"
+					@toggleAccordion="onAccordionClick">
 					<template #visible="visibleProps">
-						<nuxt-link to="/gallery" class="parent-link underline-link">
-							Gallery</nuxt-link>
+						<nuxt-link
+							to="/gallery"
+							class="parent-link underline-link">
+							Gallery</nuxt-link
+						>
 					</template>
-					<template #hidden="{ closeAccordion }">
+					<template >
 						<div class="flex flex-col">
-
-							<nuxt-link @click="closeAccordion" class="child-link whitespace-nowrap" :external="true"
-								to="/gallery#views">Views</nuxt-link>
-							<nuxt-link @click="closeAccordion" class="child-link whitespace-nowrap" :external="true" to="/gallery#food">Menu
-								Items</nuxt-link>
-							<nuxt-link @click="closeAccordion" class="child-link whitespace-nowrap" :external="true" to="/gallery#fun">Fun
-								Times</nuxt-link>
-
+							<nuxt-link
+									@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/gallery#views"
+								>Views</nuxt-link
+							>
+							<nuxt-link
+							@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/gallery#food"
+								>Menu Items</nuxt-link
+							>
+							<nuxt-link
+								@click="closeMobileMenu"
+								class="child-link whitespace-nowrap"
+								to="/gallery#fun"
+								>Fun Times</nuxt-link
+							>
 						</div>
 					</template>
 				</Accordion>
 			</li>
-
 		</ul>
 	</div>
 </template>
 
 <script setup>
+const { emit } = defineEmits(["closeMobileMenu"]);
 const data = reactive({
+	isMobileOpen: false,
 	accordionPages: [
-		{ id: 0,name: "Menu",open: false },
-		{ id: 1,name: "Gallery",open: false },
-	]
-})
-const onAccordionClick=(payload)=>{
-	data.accordionPages.forEach((item)=>(item.open = item.id === payload.id ? payload.open : false))
-}
-
+		{ id: 0, name: "Menu", open: false },
+		{ id: 1, name: "Gallery", open: false },
+	],
+});
+const onAccordionClick = (payload) => {
+	data.accordionPages.forEach(
+		(item) => (item.open = item.id === payload.id ? payload.open : false)
+	);
+};
+const closeMobileMenu = () => {
+document.querySelector(".main-page").classList.remove("mobile-menu-open")
+document.querySelector(".mobile-nav-trigger").classList.remove("bi-x")
+document.querySelector(".mobile-nav-trigger").classList.add("bi-list")
+};
 </script>
-
